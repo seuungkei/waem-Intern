6bb0135f1bfb2712365b2253bcbda8f66d88b4e5
seuungkei <seuungkei@gmail.com>
2023-02-17 08:20:04 +0900

    ADD : 상품 상세페이지 기능 구현

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -31,6 +31,38 @@ const getAllProduct = async (userId, limit, offset) => {
   }
 };
 
+const getDetailProduct = async (productId) => {
+  return await mysqlDatabase.query(
+    `
+    SELECT
+      p.id,
+      p.title,
+      p.price,
+      p.content,
+      cg.name as category,
+      u.nickname,
+      r.name AS region,
+      c.name AS city,
+      a.name AS address,
+      JSON_ARRAYAGG(
+        pi.image_url
+      ) as product_image
+    FROM
+      products p
+    INNER JOIN sellers s ON s.id = p.seller_id
+    INNER JOIN users u ON s.user_id = u.id
+    INNER JOIN regions r ON u.region_id = r.id
+    INNER JOIN cities c ON u.city_id = c.id
+    INNER JOIN address a ON u.address_id = a.id
+    INNER JOIN categories cg ON p.category_id = cg.id
+    LEFT JOIN product_images pi ON pi.product_id = p.id
+    WHERE p.id = ?
+    `,
+    [productId]
+  );
+};
+
 module.exports = {
   getAllProduct,
+  getDetailProduct,
 };
--- api/routes/productRouter.js
+++ api/routes/productRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const productController = require('../controllers/productControllers');
 
 router.get('/', productController.getAllProduct);
+router.get('/:productId', productController.getDetailProduct);
 
 module.exports = {
   router,
